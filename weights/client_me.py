511571f56435be284755948e34a14edc4a209bbe
Mian Xu <2729035426@qq.com>
2025-07-24 11:49:21 +0800

    Update client_me.py

--- weights/client_me.py
+++ weights/client_me.py
@@ -3,7 +3,7 @@ import requests
 #1	aaaaaa
 #2	bbbbbb
 #3	CCCCCC
-f = open(r"37220222203644.txt","rb")#提交的结果以自己的学号命名
+f = open(r"result.txt","rb")#提交的结果以自己的学号命名
 files = {'file': f}
 r = requests.post(url="http://101.34.251.69:5005/detectfile",files=files)#服务器地址不要修改
 print(r.text)
